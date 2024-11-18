9e11f46f71a2be68a41f2941acc6836d1bcb1a27
TomaszLizer <tomasz.lizer@anywhere.co>
2024-11-18 15:54:17 +0100

    Add CharacterDetailsView placeholder

---
+++ RickAndMorty-grupa1/Views/CharacterDetails/CharacterDetailsView.swift
@@ -0,0 +1,15 @@
+//
+//  CharacterDetailsView.swift
+//  RickAndMorty-grupa1
+//
+//  Created by Tomasz Lizer on 18/11/2024.
+//
+
+import SwiftUI
+
+struct CharacterDetailsView: View {
+    
+    var body: some View {
+        Text("CharacterDetailsView")
+    }
+}
